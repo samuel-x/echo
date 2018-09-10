aa7e63306f52d48a912733230096e83370f39ce0
Max Philip <maxp4652@gmail.com>
2018-09-11 00:36:46 +1000

    nav screen basic functionality

--- Echo/app/src/main/java/com/unimelb/droptable/echo/activities/HomePlaceholder.java
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/activities/HomePlaceholder.java
@@ -1,14 +1,48 @@
 package com.unimelb.droptable.echo.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.unimelb.droptable.echo.R;
 
-public class HomePlaceholder extends AppCompatActivity {
+import android.support.v7.widget.Toolbar;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class HomePlaceholder extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_placeholder);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng melbourne = new LatLng(-37.8136, 144.9631);
+        googleMap.addMarker(new MarkerOptions().position(melbourne)
+                .title("Marker in Melbourne"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
+    }
+
 }
