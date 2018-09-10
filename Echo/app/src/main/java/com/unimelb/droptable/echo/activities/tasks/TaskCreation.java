96047e186e0744c3a4a0cdce56ab8df50840a692
Samuel Xu (samuel-x) <roflollies@gmail.com>
2018-09-10 16:22:27 +1000

    Added 5 new non-functioning activities which will represent the flow of creating new tasks. This involves new strings, layouts and styles.

---
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/activities/tasks/TaskCreation.java
@@ -0,0 +1,14 @@
+package com.unimelb.droptable.echo.activities.tasks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.unimelb.droptable.echo.R;
+
+public class TaskCreation extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_creation_start);
+    }
+}
