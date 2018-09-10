14ab3aaa47ca62478df0601c910722ce21fbbffb
Alexander Terp <alexander.terp@gmail.com>
2018-09-10 12:54:53 +1000

    Add a simple sign in screen
    Sets a username value in ClientInfo.java as well to remember the
    username that was user to login with.

---
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/ClientInfo.java
@@ -0,0 +1,13 @@
+package com.unimelb.droptable.echo;
+
+public class ClientInfo {
+    private static String username;
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String _username) {
+        username = _username;
+    }
+}
---
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/activities/LoginActivity.java
@@ -0,0 +1,109 @@
+package com.unimelb.droptable.echo.activities;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.unimelb.droptable.echo.ClientInfo;
+import com.unimelb.droptable.echo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+    private static final int MIN_USERNAME_LENGTH = 4;
+
+    // UI references.
+    private EditText usernameText;
+    private Button signInButton;
+    private View signInView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        usernameText = findViewById(R.id.usernameText);
+
+        // Get a reference to the sign in button and set its listener.
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener((view) -> {attemptLogin();});
+
+        signInView = findViewById(R.id.signInView);
+    }
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        showProgress(true);
+
+        String username = usernameText.getText().toString();
+        if (!isUsernameValid(username)) {
+            showProgress(false);
+        }
+
+        ClientInfo.setUsername(username);
+        // TODO: Switch activity.
+    }
+
+    private boolean isUsernameValid(String username) {
+        return username.length() >= MIN_USERNAME_LENGTH;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            signInView.setVisibility(show ? View.GONE : View.VISIBLE);
+            signInView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    signInView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            signInView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+}
+
