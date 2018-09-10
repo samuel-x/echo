8f15e8bdcb76d9956581fa446a7bc63bde7cdd13
Alexander Terp <alexander.terp@gmail.com>
2018-09-10 18:01:50 +1000

    Add standardized font sizes
    Adds standardized font sizes for APs and assistants. These are defined
    inside values/dimens.xml. The intention is that all font sizes in the
    app are defined in terms of these. There are two standards: one for APs
    and one for assistants. The idea is that screens/text that is intended
    for APs utilize the AP standard, and text for assistants will utilize
    the assistant standard.
    Also fix bug where you could log in despite a short password.

--- Echo/app/src/main/java/com/unimelb/droptable/echo/activities/login/LoginActivity.java
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/activities/login/LoginActivity.java
@@ -61,6 +61,7 @@ public class LoginActivity extends AppCompatActivity {
         String username = usernameText.getText().toString();
         if (!isUsernameValid(username)) {
             showProgress(false);
+            return;
         }
 
         // The login attempt is valid. Remember the entered information.
