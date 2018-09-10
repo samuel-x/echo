0fcd0c2874865b444ff43be5a1bb799d1c10f03f
Samuel Xu (samuel-x) <roflollies@gmail.com>
2018-09-10 17:48:36 +1000

    Fixed a spelling error, oops!

--- Echo/app/src/main/java/com/unimelb/droptable/echo/activities/tasks/TaskConfirm.java
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/activities/tasks/TaskConfirm.java
@@ -47,7 +47,6 @@ public class TaskConfirm extends AppCompatActivity {
     private void setRecent() {
         Log.d("test","recent");
     }
-W
     private void confirmSubmit() {
         startActivity(new Intent(null, HomePlaceholder.class));
 
