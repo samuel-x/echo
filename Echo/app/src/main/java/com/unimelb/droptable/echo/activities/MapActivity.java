75cce72f0c54de1a91c153377fd49f756f3c1f62
Max Philip <maxp4652@gmail.com>
2018-09-27 14:19:54 +1000

    routing from device location to specified destination works - but not updating properly

--- Echo/app/src/main/java/com/unimelb/droptable/echo/activities/MapActivity.java
+++ Echo/app/src/main/java/com/unimelb/droptable/echo/activities/MapActivity.java
@@ -1,14 +1,17 @@
 package com.unimelb.droptable.echo.activities;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,6 +31,7 @@ import com.google.maps.model.DirectionsRoute;
 import com.google.maps.model.DirectionsStep;
 import com.google.maps.model.EncodedPolyline;
 import com.unimelb.droptable.echo.R;
+import com.unimelb.droptable.echo.activities.tasks.TaskCreation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,14 +41,35 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private GoogleMap mMap;
     private String TAG = "so47492459";
 
+    private FloatingActionButton newTaskButton;
+
+    private FloatingActionButton settingsButton;
+    private FloatingActionButton infoButton;
+    private Button paymentButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        newTaskButton = findViewById(R.id.addTaskButton);
+        newTaskButton.setOnClickListener((view) -> {
+            newTask();
+        });
+
+        // currently placeholders
+        settingsButton = findViewById(R.id.settingsButton);
+        infoButton = findViewById(R.id.infoButton);
+        paymentButton = findViewById(R.id.paymentButton);
+        paymentButton.setOnClickListener((view) -> {
+            toPayment();
+        });
     }
 
 
@@ -65,21 +90,20 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         // WORKSHOP LOCATION FOR TESTING: -37.800900, 144.958847
 
         LatLng melbourne = new LatLng(-37.8136, 144.9631);
-        //mMap.addMarker(new MarkerOptions().position(melbourne).title("Marker in Melbourne"));
 
-        LatLng fitzroy = new LatLng(-37.8011,144.9789);
-        //mMap.addMarker(new MarkerOptions().position(fitzroy).title("Marker in Fitzroy"));
+        LatLng fitzroy = new LatLng(-37.8011, 144.9789);
 
-        LatLng carlton = new LatLng( -37.8001, 144.9671);
-        //mMap.addMarker(new MarkerOptions().position(carlton).title("Marker in Carlton"));
+        LatLng carlton = new LatLng(-37.8001, 144.9671);
 
-        LatLng docklands = new LatLng (-37.8170, 144.9460);
-        mMap.addMarker(new MarkerOptions().position(docklands).title("Destination: Docklands"));
-        LatLng southbank = new LatLng (-37.8290, 144.9570);
-        //mMap.addMarker(new MarkerOptions().position(southbank).title("Destination: Southbank"));
+        LatLng docklands = new LatLng(-37.8170, 144.9460);
+
+        LatLng southbank = new LatLng(-37.8290, 144.9570);
 
 
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        LatLng destination = new LatLng(docklands.latitude, docklands.longitude);
+        mMap.addMarker(new MarkerOptions().position(destination).title("Destination: " + "Docklands"));
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Location location;
         double longitude;
         double latitude;
@@ -88,7 +112,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             location = lm.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
             latitude = location.getLatitude();
             longitude = location.getLongitude();
-            mMap.addMarker(new MarkerOptions().position(new LatLng (latitude, longitude)).title("Start Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Start Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 
         } catch (SecurityException e) {
             Log.d("GPS_error", "Your GPS is not working");
@@ -97,10 +121,13 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             mMap.addMarker(new MarkerOptions().position(carlton).title("Default Location: Carlton"));
         }
 
+        // to center the map, need to have midpoint of start and end points
+        LatLng midPoint = new LatLng ((latitude + destination.latitude)/2, (longitude + destination.longitude)/2);
+
+
 
 
         // THIS ROUTE DRAWING BASED ON: https://stackoverflow.com/questions/47492459/android-google-maps-draw-a-route-between-two-points-along-the-road
-
 
         //Define list to get all latlng for the route
         List<LatLng> path = new ArrayList();
@@ -110,7 +137,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                 .apiKey("AIzaSyBrPt88vvoPDDn_imh-RzCXl5Ha2F2LYig")
                 .build();
         DirectionsApiRequest req = DirectionsApi.getDirections(context,
-                ("\"" + latitude + "," + longitude + "\""), ("\"" + docklands.latitude + "," + docklands.longitude + "\""));
+                ("\"" + latitude + "," + longitude + "\""), ("\"" + destination.latitude + "," + destination.longitude + "\""));
         try {
             DirectionsResult res = req.await();
 
@@ -118,14 +145,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             if (res.routes != null && res.routes.length > 0) {
                 DirectionsRoute route = res.routes[0];
 
-                if (route.legs !=null) {
-                    for(int i=0; i<route.legs.length; i++) {
+                if (route.legs != null) {
+                    for (int i = 0; i < route.legs.length; i++) {
                         DirectionsLeg leg = route.legs[i];
                         if (leg.steps != null) {
-                            for (int j=0; j<leg.steps.length;j++){
+                            for (int j = 0; j < leg.steps.length; j++) {
                                 DirectionsStep step = leg.steps[j];
-                                if (step.steps != null && step.steps.length >0) {
-                                    for (int k=0; k<step.steps.length;k++){
+                                if (step.steps != null && step.steps.length > 0) {
+                                    for (int k = 0; k < step.steps.length; k++) {
                                         DirectionsStep step1 = step.steps[k];
                                         EncodedPolyline points1 = step1.polyline;
                                         if (points1 != null) {
@@ -151,7 +178,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
                     }
                 }
             }
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             Log.e(TAG, ex.getLocalizedMessage());
         }
 
@@ -163,75 +190,14 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(melbourne, 14));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(midPoint, 14));
+    }
+
+    private void newTask() {
+        startActivity(new Intent(this, TaskCreation.class));
+    }
+
+    public void toPayment() {
+        startActivity(new Intent(this, PaymentActivity.class));
     }
 }
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.support.design.widget.FloatingActionButton;
-//import android.support.v4.app.FragmentActivity;
-//import android.widget.Button;
-//
-//import com.google.android.gms.maps.CameraUpdateFactory;
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.MapFragment;
-//import com.google.android.gms.maps.OnMapReadyCallback;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.MarkerOptions;
-//
-//import com.unimelb.droptable.echo.activities.tasks.TaskCreation;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
-//
-//    private GoogleMap mMap;
-//
-//    private FloatingActionButton newTaskButton;
-//
-//    private FloatingActionButton settingsButton;
-//    private FloatingActionButton infoButton;
-//    private Button paymentButton;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_map);
-//        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//
-//        newTaskButton = findViewById(R.id.addTaskButton);
-//        newTaskButton.setOnClickListener((view) -> {newTask();});
-//
-//        // currently placeholders
-//        settingsButton = findViewById(R.id.settingsButton);
-//        infoButton = findViewById(R.id.infoButton);
-//        paymentButton = findViewById(R.id.paymentButton);
-//        paymentButton.setOnClickListener((view) -> {toPayment();});
-//    }
-//
-//    private void newTask() {
-//        startActivity(new Intent(this, TaskCreation.class));
-//    }
-//
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        // Add a marker in Sydney, Australia,
-//        // and move the map's camera to the same location.
-//        LatLng melbourne = new LatLng(-37.8136, 144.9631);
-//
-//        googleMap.addMarker(new MarkerOptions().position(melbourne)
-//                .title("Marker in Melbourne"));
-//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(melbourne));
-//
-//        googleMap.setMinZoomPreference(12);
-//
-//    }
-//
-//    public void toPayment() {
-//        startActivity(new Intent(this, PaymentActivity.class));
-//    }
-//
-//}
